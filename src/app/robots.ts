d3d4e0e9527d1902b1ca4d42a901148227133ddb
Pragvance <stilwellmark@gmail.com>
2026-09-14 04:52:12 -0700

    chore: noindex disallow /md-map and /progress-map in robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/login", "/dashboard"],
+        disallow: ["/login", "/dashboard", "/md-map", "/progress-map"],
       },
     ],
     sitemap: `${SITE.url}/sitemap.xml`,
